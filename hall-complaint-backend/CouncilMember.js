d20a9d69395054065b017758cf57d72950b23376
Kapil <jaimatadi364636@gmail.com>
2026-04-06 01:48:33 +0530

    error fixed

---
+++ hall-complaint-backend/CouncilMember.js
@@ -0,0 +1,8 @@
+const councils = [
+  { name: "Kapil", roll: "24IM10020" , "por":"GSec Maintainance", hall: "RK Hall", email:"kc3806716@gmail.com" },
+//   { name: "Amit", roll: "24AE10020", hall: "MS Hall", email:"choudharyamit9887.24@kgpian.iitkgp.ac.in" },
+//   { name: "Ankit", roll: "24IM10015", hall: "Nehru Hall", email:"ankit123.24@kgpian.iitkgp.ac.in" },
+//   { name: "Anshuman", roll: "24IM10028", hall: "RK Hall", email:"anshuman.24@kgpian.iitkgp.ac.in" },
+];
+
+module.exports = councils;
