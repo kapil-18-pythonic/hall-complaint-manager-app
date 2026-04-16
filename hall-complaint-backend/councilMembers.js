a2f0fc985d38433d41de1a908f08bffd40e9a266
Kapil <jaimatadi364636@gmail.com>
2026-04-16 14:34:08 +0530

    data changed

--- hall-complaint-backend/councilMembers.js
+++ hall-complaint-backend/councilMembers.js
@@ -1,7 +1,7 @@
 const councils = [
-  { name: "Kapil", roll: "24IM10020" , "por":"GSec Maintainance", hall: "RK Hall", email:"kc3806716@gmail.com" },
-  { name: "Amit", roll: "24IM10028", hall: "RK Hall","por":"GSec Sports" email:"choudharyamit9887.24@kgpian.iitkgp.ac.in" },
-  { name: "Ankit", roll: "24IM10015", hall: "RK Hall","por":"GSec Mess" email:"ankit123.24@kgpian.iitkgp.ac.in" },
+  { name: "Kapil", roll: "24IM10020" , por:"GSec Maintainance", hall: "RK Hall", email:"kc3806716@gmail.com" },
+  { name: "Amit", roll: "24IM10028", hall: "RK Hall",por:"GSec Sports" ,email:"choudharyamit9887.24@kgpian.iitkgp.ac.in" },
+  { name: "Ankit", roll: "24IM10015", hall: "RK Hall",por:"GSec Mess" ,email:"ankit123.24@kgpian.iitkgp.ac.in" },
 //   { name: "Anshuman", roll: "24IM10028", hall: "RK Hall", email:"anshuman.24@kgpian.iitkgp.ac.in" },
 ];
 
